7c1c61791fe3cc2447866ad42a717ec0d6510419
Alvaro <pursuance@gmail.com>
2015-10-11 20:31:57 +0200

    Changed inicio to terminator and deleted some temps

--- inicio.sh
+++ inicio.sh
@@ -6,4 +6,4 @@ wmctrl -o 2000,0
 google-chrome http://localhost:3000 "--user-data-dir=${HOME}/.google-chrome/session${DISPLAY}" &
 sleep 1
 wmctrl -o 0,2000
-gnome-terminal --window --maximize --working-directory='~/code/Bluesmile' &
+terminator --borderless --maximise --working-directory='~/code/Bluesmile' &
